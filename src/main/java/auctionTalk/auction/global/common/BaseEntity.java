2ffb03cafa9b8d426c57c8e9aac7140660a7b8b6
hig1002 <hig7463@naver.com>
2025-09-06 23:55:38 +0900

    feat: BaseEntity 생성

--- src/main/java/auctionTalk/auction/global/common/BaseEntity.java
+++ src/main/java/auctionTalk/auction/global/common/BaseEntity.java
@@ -1,4 +1,36 @@
 package auctionTalk.auction.global.common;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    // 삭제 여부 확인 메서드
+    public boolean isDeleted() {
+        return deletedAt != null;
+    }
+
+    // 삭제 처리 메서드
+    public void delete() {
+        deletedAt = LocalDateTime.now();
+    }
 }
