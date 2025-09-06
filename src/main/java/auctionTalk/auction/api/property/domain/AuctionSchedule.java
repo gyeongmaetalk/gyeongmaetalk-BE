50de0575ca883ec449906623f2b492d88ff2761d
hig1002 <hig7463@naver.com>
2025-09-06 23:58:06 +0900

    feat: Property 엔티티 생성

--- src/main/java/auctionTalk/auction/api/property/domain/AuctionSchedule.java
+++ src/main/java/auctionTalk/auction/api/property/domain/AuctionSchedule.java
@@ -1,4 +1,30 @@
 package auctionTalk.auction.api.property.domain;
 
-public class AuctionSchedule {
+import auctionTalk.auction.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AuctionSchedule extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+
+    private Integer round;
+
+    private Integer minPrice;
+
+    private String result;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Property property;
 }
--- src/main/java/auctionTalk/auction/api/property/domain/PropertyImage.java
+++ src/main/java/auctionTalk/auction/api/property/domain/PropertyImage.java
@@ -1,4 +1,23 @@
 package auctionTalk.auction.api.property.domain;
 
-public class PropertyImage {
+import auctionTalk.auction.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PropertyImage extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Property property;
 }
