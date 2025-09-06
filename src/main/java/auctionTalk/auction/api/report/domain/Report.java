f4e8e922fc4a66e7ead31ae3ab201074993d8f65
hig1002 <hig7463@naver.com>
2025-09-06 23:59:11 +0900

    feat: Report 엔티티 생성

--- src/main/java/auctionTalk/auction/api/report/domain/Report.java
+++ src/main/java/auctionTalk/auction/api/report/domain/Report.java
@@ -1,4 +1,29 @@
 package auctionTalk.auction.api.report.domain;
 
-public class Report {
+import auctionTalk.auction.api.counselor.domain.Counselor;
+import auctionTalk.auction.api.user.domain.User;
+import auctionTalk.auction.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Counselor counselor;
+
+    private String content;
+
 }
