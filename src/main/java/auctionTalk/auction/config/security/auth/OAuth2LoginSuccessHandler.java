29526cac74d8be9ea8c1dcaf25f53040167b331b
hig1002 <hig7463@naver.com>
2025-10-04 06:41:33 +0900

    fix: 컴포넌트 주입 누락 추가

--- src/main/java/auctionTalk/auction/config/security/auth/OAuth2LoginSuccessHandler.java
+++ src/main/java/auctionTalk/auction/config/security/auth/OAuth2LoginSuccessHandler.java
@@ -8,10 +8,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
